73685f46479024e600a1f26aac0d4d6c8c03a08f
Liis R <liisvoks@gmail.com>
2022-11-02 16:54:40 +0200

    komment

---
+++ src/components/customer_table/CustomersTableBody.vue
@@ -0,0 +1,33 @@
+<template>
+  <tbody>
+  <tr v-for="customer in customers" >
+    <th scope="row">*</th>
+    <td>{{customer.firstName}}</td>
+    <td>{{customer.lastName}}</td>
+    <td>{{customer.personalCode}}</td>
+
+  </tr>
+  </tbody>
+</template>
+<script>
+export default {
+  name: 'CustomersTableBody',
+  data: function () {
+    return{
+      customers: [
+        {
+          firstName: `Rain`,
+          lastName: `Tüür`,
+          personalCode: `5456666`
+        },
+        {
+          firstName: `Kaupo`,
+          lastName: `Saar`,
+          personalCode: `335456666`
+        }
+      ]
+    }
+
+  }
+}
+</script>

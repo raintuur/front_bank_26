139666261394e1f9744cbf11f18fa3b6cb0729c6
Valeria <valeria.lukashevich@gmail.com>
2022-11-02 16:20:02 +0200

    commit

---
+++ src/components/CustomersTable.vue
@@ -0,0 +1,31 @@
+<template>
+  <table class="table table-dark table-striped">
+    <thead>
+    <tr>
+      <th scope="col">#</th>
+      <th scope="col">Eesnimi</th>
+      <th scope="col">Perekonnanimi</th>
+      <th scope="col">Isikukood</th>
+    </tr>
+    </thead>
+    <tbody>
+    <tr>
+      <th scope="row">1</th>
+      <td>Mark</td>
+      <td>Otto</td>
+      <td>@mdo</td>
+    </tr>
+    <tr>
+      <th scope="row">2</th>
+      <td>Jacob</td>
+      <td>Thorntl</td>
+      <td>@fat</td>
+    </tr>
+    </tbody>
+  </table>
+</template>
+<script>
+export default {
+  name: 'CustomersTable'
+}
+</script>
---
+++ src/views/CustomersView.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="row m-5">
+
+    <CustomersTable/>
+
+  </div>
+</template>
+
+<script>
+import CustomersTable from "@/components/CustomersTable";
+
+export default {
+  name: "CustomersView",
+  components: {CustomersTable}
+}
+</script>
+

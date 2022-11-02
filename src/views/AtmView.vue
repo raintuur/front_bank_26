d1f0a74317c068e6eb4730fb5be8d9ed913f150b
Mihke <mihkelviirsalu@hotmail.com>
2022-11-02 14:34:04 +0200

    Väike muutus AtmView's

--- src/views/AtmView.vue
+++ src/views/AtmView.vue
@@ -1,28 +1,48 @@
 <template>
   <div>
-
+Tere maailm
     <div class="d-flex justify-content-center">
       <div class="row">
         <div class="col">
-          <input type="text" class="form-control" placeholder="First name" aria-label="First name">
+          <input v-model="firstName" type="text" class="form-control" placeholder="First name" aria-label="First name">
         </div>
         <div class="col">
-          <input type="text" class="form-control" placeholder="Last name" aria-label="Last name">
+          <input v-model="lastName" class="form-control" placeholder="Last name" aria-label="Last name">
         </div>
       </div>
+
     </div>
+    <input :value="firstName">
+    <br>
+    <br>
+    Eesnimi: {{firstName}}
+    <br>
+    Perekonnanimi:{{lastName}}
+
+
+
+
+
+
 
   </div>
 </template>
 
 <script>
 export default {
-  name: "AtmView"
+  name: 'AtmView',
+  data: function () {
+    return {
+      firstName: '',
+      lastName: ''
+    }
+
+  },
+
+
+
 }
 
 
 </script>
 
-<style scoped>
-
-</style>

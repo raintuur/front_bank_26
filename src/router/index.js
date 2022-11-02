d9718e183a66bbd266762bab1ef822d7968c6b05
Rain Tüür <rain.tuur@gmail.com>
2022-11-02 10:13:38 +0200

    Added ATM

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import HomeView from '../views/HomeView.vue'
+import AtmView from "@/views/AtmView";
 
 Vue.use(VueRouter)
 
@@ -9,6 +10,11 @@ const routes = [
     path: '/',
     name: 'home',
     component: HomeView
+  },
+  {
+    path: '/atm',
+    name: 'atmRoute',
+    component: AtmView
   },
   {
     path: '/about',
---
+++ src/views/AtmView.vue
@@ -0,0 +1,17 @@
+<template>
+<div>
+  tere maailm asdasdasd
+</div>
+</template>
+
+<script>
+export default {
+  name: "AtmView"
+}
+
+
+</script>
+
+<style scoped>
+
+</style>

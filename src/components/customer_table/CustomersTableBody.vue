eb3be7bfe2efa446839d1733bf4e2fd557ca79f8
temp <astrid.oras@gmail.com>
2022-11-02 16:57:20 +0200

    teine

---
+++ src/components/customer_table/CustomersTableBody.vue
@@ -0,0 +1,38 @@
+<template>
+  <tbody>
+  <tr v-for="customer in customers">
+    <th scope="row">*</th>
+    <td>{{customer.firstName}}</td>
+    <td>{{customer.lastName}}</td>
+    <td>{{customer.personalcode}}</td>
+
+  </tr>
+  </tbody>
+</template>
+<script>
+export default {
+  name: 'CustomersTableBody',
+  data: function () {
+    return {
+      customers: [
+        {
+          firstName: 'Rain',
+          lastName: 'Tüür',
+          personalcode: '38901220277'
+        },
+        {
+          firstName: 'Kaupo',
+          lastName: 'Tüür',
+          personalcode: '38701220277'
+        },
+        {
+          firstName: 'Astrid',
+          lastName: 'Oras',
+          personalcode: '48901220277'
+        },
+    }
+      ]
+    }
+  }
+}
+</script>

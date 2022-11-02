a806f22ccc3e955f69e84526a2a1747ed52fc693
Mario Mumm <mario.mumm@hotmail.com>
2022-11-02 16:19:50 +0200

    Commit

--- src/views/CustomersView.vue
+++ src/views/CustomersView.vue
@@ -1,40 +1,20 @@
 <template>
   <div>
     <div class="row m-5">
-      <table class="table table-dark table-striped m-auto">
-        <thead>
-        <tr>
-          <th scope="col">#</th>
-          <th scope="col">First</th>
-          <th scope="col">Last</th>
-          <th scope="col">Handle</th>
-        </tr>
-        </thead>
-        <tbody>
-        <tr>
-          <th scope="row">1</th>
-          <td>Mark</td>
-          <td>Otto</td>
-          <td>@mdo</td>
-        </tr>
-        <tr>
-          <th scope="row">2</th>
-          <td>Jacob</td>
-          <td>Thornton</td>
-          <td>@fat</td>
-        </tr>
-        </tbody>
-      </table>
+
+      <CustomersTable/>
+
     </div>
   </div>
+
 </template>
 
 <script>
+import CustomersTable from "@/components/CustomersTable";
+
 export default {
-  name: "CustomersView"
+  name: "CustomersView",
+  components: {CustomersTable}
 }
 </script>
 
-<style scoped>
-
-</style>

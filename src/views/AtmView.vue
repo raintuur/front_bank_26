384117c891d73ec0b38ec9ecda909f09fd7580f8
Mairold <mairold.maegi@gmail.com>
2022-11-02 16:19:25 +0200

    Small change in AtmView message and CustomersView added.

--- src/views/AtmView.vue
+++ src/views/AtmView.vue
@@ -1,28 +1,45 @@
 <template>
   <div>
 
-    <div class="d-flex justify-content-center">
-      <div class="row">
-        <div class="col">
-          <input type="text" class="form-control" placeholder="First name" aria-label="First name">
+    <div class="container">
+      <div class="row justify-content-center">
+        <div class="col col-lg-2">
+          <input v-model="firstName" type="text" class="form-control" placeholder="First name" aria-label="First name">
         </div>
-        <div class="col">
-          <input type="text" class="form-control" placeholder="Last name" aria-label="Last name">
+        <div class="col col-lg-2">
+          <input v-model="lastName" type="text" class="form-control" placeholder="Last name" aria-label="Last name">
+        </div>
+      </div>
+      <div class="row justify-content-md-center">
+        <div class="col col-lg-2 m-2">
+          <button v-on:click="helloWorld('Nipi','Tiri')" type="button" class="btn btn-lg  btn-outline-info">Info</button>
         </div>
       </div>
     </div>
+
+
+
 
   </div>
 </template>
 
 <script>
 export default {
-  name: "AtmView"
+  name: 'AtmView',
+
+  data: function () {
+    return {
+      firstName: '',
+      lastName: ''
+    }
+  },
+  methods: {
+    helloWorld: function (firstName, lastname) {
+      alert('Hello World! ' + this.firstName + ' ' + this.lastName)
+    }
+  }
+
 }
 
 
 </script>
-
-<style scoped>
-
-</style>

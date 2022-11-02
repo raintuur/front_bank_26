f440da81eeb03eb138c3cf79317cd49ab46e08cc
Mario Mumm <mario.mumm@hotmail.com>
2022-11-02 16:50:11 +0200

    Commit

---
+++ src/components/customer_table/CustomerTableBody.vue
@@ -0,0 +1,36 @@
+<template>
+  <tbody>
+  <tr v-for="customer in customers">
+    <th scope="row">*</th>
+    <td>{{customer.firstName}}</td>
+    <td>{{customer.lastName}}</td>
+    <td>{{customer.personalCode}}</td>
+  </tr>
+  </tbody>
+</template>
+<script>
+export default {
+  name: 'CustomerTableBody',
+  data: function () {
+    return {
+      customers: [
+        {
+          firstName: 'Mario',
+          lastName: 'Mumm',
+          personalCode: '3940620'
+        },
+        {
+          firstName: 'Kaja',
+          lastName: 'Vaher',
+          personalCode: '487615'
+        },
+        {
+          firstName: 'Kaupo',
+          lastName: 'Vaher',
+          personalCode: '3670327'
+        }
+      ]
+    }
+  }
+}
+</script>
---
+++ src/components/customer_table/CustomersTable.vue
@@ -0,0 +1,15 @@
+<template>
+  <table class="table table-dark table-hover">
+    <CustomerTableHead/>
+    <CustomerTableBody/>
+  </table>
+</template>
+<script>
+import CustomerTableBody from "@/components/customer_table/CustomerTableBody";
+import CustomerTableHead from "@/components/customer_table/CustomerTableHead";
+
+export default {
+  name: 'CustomersTable',
+  components: {CustomerTableHead, CustomerTableBody}
+}
+</script>

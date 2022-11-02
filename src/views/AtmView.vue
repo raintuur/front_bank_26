dbfaf2f2882e66f6aba832a18ab619242a04dca2
Valeria <valeria.lukashevich@gmail.com>
2022-11-02 14:34:30 +0200

    commit

--- src/views/AtmView.vue
+++ src/views/AtmView.vue
@@ -4,25 +4,35 @@
     <div class="d-flex justify-content-center">
       <div class="row">
         <div class="col">
-          <input type="text" class="form-control" placeholder="First name" aria-label="First name">
+          <input v-model="firstName" type="text" class="form-control" placeholder="First name" aria-label="First name">
         </div>
         <div class="col">
-          <input type="text" class="form-control" placeholder="Last name" aria-label="Last name">
+          <input v-model="lastName" type="text" class="form-control" placeholder="Last name" aria-label="Last name">
         </div>
       </div>
     </div>
+
+    <input :value="firstName">
+    <br>
+    Eesnimi:{{firstName}}
+    <br>
+    Perekonnanimi:{{lastName}}
 
   </div>
 </template>
 
 <script>
 export default {
-  name: "AtmView"
+  name: 'AtmView',
+  data: function () {
+    return {
+      firstName: '',
+      lastName: ''
+    }
+
+  }
 }
 
 
 </script>
 
-<style scoped>
-
-</style>

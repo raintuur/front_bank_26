efb342a2348ec45cc8e450cf2f4770547516cb19
AnetteKy <anette.kytt@gmail.com>
2022-11-02 16:20:19 +0200

    commit

---
+++ src/components/CustomersTable.vue
@@ -0,0 +1,25 @@
+<template>
+  <table class="table table-dark table-hover" style="width: 70%" align="center">
+    <thead>
+    <tr>
+      <th scope="col">#</th>
+      <th scope="col">Eesnimi</th>
+      <th scope="col">Perekonnanimi</th>
+      <th scope="col">Isikukood</th>
+    </tr>
+    </thead>
+    <tbody>
+    <tr>
+      <th scope="row">1</th>
+      <td>Mark</td>
+      <td>Otto</td>
+      <td>@mdo</td>
+    </tr>
+    </tbody>
+  </table>
+</template>
+<script>
+export default {
+  name: 'CustomersTable'
+}
+</script>

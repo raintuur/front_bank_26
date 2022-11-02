f8fec704562656cdd1384a4aa4be89f08f81c926
Andres Orgla <aorgla@gmail.com>
2022-11-02 14:33:01 +0200

    commit

--- src/views/AtmView.vue
+++ src/views/AtmView.vue
@@ -4,25 +4,28 @@
     <div class="d-flex justify-content-center">
       <div class="row">
         <div class="col">
-          <input type="text" class="form-control" placeholder="First name" aria-label="First name">
+          <input v-model="firstName" type="text" class="form-control" placeholder="Eesnimi" aria-label="First name">
         </div>
         <div class="col">
-          <input type="text" class="form-control" placeholder="Last name" aria-label="Last name">
+          <input v-model="lastName" type="text" class="form-control" placeholder="Perekonnanimi" aria-label="Last name">
         </div>
       </div>
+
     </div>
 
   </div>
 </template>
 
 <script>
+
 export default {
-  name: "AtmView"
+  name: 'AtmView',
+  data: function () {
+    return {
+      firstName: '',
+      lastName: ''
+    }
+  }
 }
 
-
 </script>
-
-<style scoped>
-
-</style>

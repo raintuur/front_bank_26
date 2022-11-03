f8232f7b9632ae09706295ea4546367d367d6efb
raili-git <raili.moim@gmail.com>
2022-11-03 15:33:58 +0200

    commit3

--- src/components/CustomersTableBody.vue
+++ src/components/CustomersTableBody.vue
@@ -1,18 +1,21 @@
 <template>
   <tbody>
   <tr v-for="customer in customers">
-    <th scope="row">*</th>
+    <th scope="row">{{ customer.sequenceNumber }}</th>
     <td>{{ customer.firstName }}</td>
     <td>{{ customer.lastName }}</td>
     <td>{{ customer.personalCode }}</td>
+    <td>
+      <button v-on:click="alertPersonalCode(customer.personalCode)" type="button" class="btn btn-light">Light</button>
+    </td>
   </tr>
   </tbody>
 </template>
 <script>
 export default {
   name: 'CustomersTableBody',
-  data: function (){
-    return{
+  data: function () {
+    return {
       customers: [
         {
           firstName: 'Rain',
@@ -29,9 +32,24 @@ export default {
           lastName: 'Vaher',
           personalCode: '384'
         }
-
       ]
     }
+  },
+  methods: {
+    alertPersonalCode: function (personalCode) {
+      alert('Isikukood: ' + personalCode)
+    }
+  },
+  beforeMount() {
+    // this.customers.forEach(value => )
+    // for (let i = 0; i < this.customers.length; i++) {
+    //   this.customers[i].sequenceNumber = i + 1;
+    // }
+    let counter = 1
+    console.log('OLEN SIIN')
+    this.customers.forEach(customer => {
+      customer.sequenceNumber = counter++
+    })
   }
 }
 </script>
---
+++ src/views/CarsView.vue
@@ -0,0 +1,79 @@
+<template>
+  <div>
+    <!--    Brand loop-->
+    <div v-for="brand in carBrands" class="row justify-content-center">
+      <h6>{{ brand.brandName }}</h6>
+      <div v-for="model in brand.models" class="row">
+
+        <div class="col">
+          {{ model.modelName }}
+        </div>
+        <div class="col">
+          {{ model.year }}
+        </div>
+        <!--          <div class="col">-->
+        <!--            Veerg 3-->
+        <!--          </div>-->
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'CarsView',
+  data: function () {
+    return {
+      carBrands: [
+        {
+          brandName: 'Audi',
+          models: [
+            {
+              modelName: 'A3',
+              year: 2011
+            },
+            {
+              modelName: 'A4',
+              year: 2012
+            },
+            {
+              modelName: 'A6-RS',
+              year: 2013
+            }
+          ]
+        },
+        {
+          brandName: 'Opel',
+          models: [
+            {
+              modelName: 'Astra',
+              year: 2011
+            },
+            {
+              modelName: 'Omega',
+              year: 2012
+            }
+          ]
+        },
+        {
+          brandName: 'Skoda',
+          models: [
+            {
+              modelName: 'Fabia',
+              year: 2011
+            },
+            {
+              modelName: 'Superb',
+              year: 2012
+            }
+          ]
+        }
+      ]
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>

c6d70d9bf4bc4feeb95cbc3a8834e48164d41141
Sandra Sulp <sandra.sulp@gmail.com>
2022-11-04 12:46:44 +0200

    Commit- fixed citiesdown missing key,refactored CitiesDropdown component. Added ServicesCheckbox  component.

--- src/views/AtmView.vue
+++ src/views/AtmView.vue
@@ -4,55 +4,42 @@
     <div class="container">
       <div class="row justify-content-start">
         <div class="col col-lg-3">
-          <select v-model= "selectedCityId" class="form-select" aria-label="Default select example">
-            <option selected disabled value="0">--Linn--</option>
-            <option v-for="city in cities" :value="city.cityNameId">{{city.cityName}}</option>
-          </select>
 
+          <CitiesDropdown/>
+
+          <div class="row">
+
+            <ServicesCheckbox/>
+
+
+
+          </div>
         </div>
       </div>
     </div>
+
+
 
 
   </div>
 </template>
 
 <script>
+import CitiesDropdown from "@/components/CitiesDropdown";
+import ServicesCheckbox from "@/components/ServicesCheckbox";
+
 export default {
   name: 'AtmView',
-
+  components: {ServicesCheckbox, CitiesDropdown},
   data: function () {
     return {
-      selectedCityId: 0,
-      firstName: '',
-      cities:  [
-        {
-          cityName: '',
-          cityNameId: 0
-        }
-      ]
+
     }
   },
   methods: {
 
-   getCitiesSelectBoxInfo: function () {
-
-     this.$http.get('/atm/city')
-         .then(result => {
-           this.cities = result.data
-           console.log('CITIES: ' + JSON.stringify(this.cities))
-         })
-         .catch(error => {
-           alert('VIGA!')
-           console.log('Oh no!')
-         });
-   }
   },
-  beforeMount() {
 
-    this.getCitiesSelectBoxInfo()
-
-  }
 }
 
 

a1689137108bda3b0bb8d63981bd7de6763bb2a3
Rain Tüür <rain.tuur@gmail.com>
2022-11-04 15:25:06 +0200

    Fixed cities dropdownd missing key, refactored CitiesDropdown to sepearate component, addded ServicesCheckbox component (HTML only).

--- src/components/ServicesCheckbox.vue
+++ src/components/ServicesCheckbox.vue
@@ -1,21 +1,42 @@
 <template>
   <div>
     <div class="form-check">
-      <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault">
+      <input class="form-check-input" type="checkbox" id="flexCheckDefault">
       <label class="form-check-label" for="flexCheckDefault">
         Default checkbox
       </label>
     </div>
-    <div class="form-check">
-      <input class="form-check-input" type="checkbox" value="" id="flexCheckChecked" checked>
-      <label class="form-check-label" for="flexCheckChecked">
-        Checked checkbox
-      </label>
-    </div>
+
   </div>
 </template>
 <script>
 export default {
-  name: 'ServicesCheckbox'
+  name: 'ServicesCheckbox',
+  data: function () {
+    return {
+      atmServices: [
+        {
+          serviceId: 0,
+          serviceName: '',
+          isSelected: false
+        }
+      ]
+    }
+  },
+  methods: {
+
+    getAtmServicesCheckboxInfo: function () {
+      this.$http.get('/atm/service')
+          .then(result => {
+            this.atmServices = result.data
+          })
+          .catch(error => {
+            alert("NO!!!!")
+          });
+    }
+  },
+  beforeMount() {
+    this.getAtmServicesCheckboxInfo()
+  }
 }
 </script>

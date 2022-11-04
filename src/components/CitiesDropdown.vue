e40150290fcfb6e6b3ac78cee63e0627401a4dab
kristjanparna <parna.kristjan@gmail.com>
2022-11-04 12:47:11 +0200

    Fixed cities dropdown missing key, refactored CitiesDropdown.vue to separate component, added ServicesCheckbox.vue component

---
+++ src/components/CitiesDropdown.vue
@@ -0,0 +1,45 @@
+<template>
+  <div>
+    <select v-model="selectedCityId" v-on:change="refreshAtmsByCity('Hello')" class="form-select"
+            aria-label="Default select example">
+      <option value="0" selected disabled>--Linn--</option>
+      <option v-for="city in cities" :key="city.cityNameId" :value="city.cityNameId">{{ city.cityName }}</option>
+    </select>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'CitiesDropdown',
+  data: function () {
+    return {
+      selectedCityId: 0,
+      cities: [
+        {
+          cityName: '',
+          cityNameId: 0
+        }
+      ]
+    }
+  },
+  methods: {
+    refreshAtmsByCity: function (message) {
+      console.log(message)
+    },
+    getCitiesSelectBoxInfo: function () {
+      this.$http.get('/atm/city')
+          .then(result => {
+            this.cities = result.data
+            console.log('Cities= ' + JSON.stringify(this.cities))
+          })
+          .catch(error => {
+            alert('Viga')
+            console.log('You screwed up :(')
+          });
+    }
+  },
+  beforeMount() {
+    this.getCitiesSelectBoxInfo()
+  }
+}
+</script>
--- src/views/AtmView.vue
+++ src/views/AtmView.vue
@@ -3,11 +3,14 @@
     <div class="container">
       <div class="row justify-content-start">
         <div class="col col-lg-3">
-          <select v-model="selectedCityId" v-on:change="refreshAtmsByCity('Hello')" class="form-select"
-                  aria-label="Default select example">
-            <option value="0" selected disabled>--Linn--</option>
-            <option v-for="city in cities" :value="city.cityNameId">{{ city.cityName }}</option>
-          </select>
+
+          <CitiesDropdown/>
+
+          <div class="row">
+
+            <ServicesCheckbox/>
+
+          </div>
         </div>
       </div>
     </div>
@@ -15,38 +18,16 @@
 </template>
 
 <script>
+import CitiesDropdown from "@/components/CitiesDropdown";
+import ServicesCheckbox from "@/components/ServicesCheckbox";
+
 export default {
   name: 'AtmView',
+  components: {ServicesCheckbox, CitiesDropdown},
   data: function () {
-    return {
-      selectedCityId: 0,
-      cities: [
-        {
-          cityName: '',
-          cityNameId: 0
-        }
-      ]
-    }
+
   },
-  methods: {
-    refreshAtmsByCity: function (message) {
-      alert(message)
-    },
-    getCitiesSelectBoxInfo: function () {
-      this.$http.get('/atm/city')
-          .then(result => {
-            this.cities = result.data
-            console.log('Cities= ' + JSON.stringify(this.cities))
-          })
-          .catch(error => {
-            alert('Viga')
-            console.log('You screwed up :(')
-          });
-    }
-  },
-  beforeMount() {
-    this.getCitiesSelectBoxInfo()
-  }
+  methods: {}
 }
 
 </script>

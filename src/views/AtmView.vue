073cd4fb5f3a18c7ce0e8261aef52672c3299427
Liis R <liisvoks@gmail.com>
2022-11-04 11:17:07 +0200

    kommit

--- src/views/AtmView.vue
+++ src/views/AtmView.vue
@@ -3,7 +3,8 @@
 
     <div class="container">
       <div class="row justify-content-start">
-        <div class="col col-lg-2">
+        <div class="col col-lg-3">
+
           <select v-model="selectedCityID" class="form-select" aria-label="Default select example">
             <option  selected disabled>--Linn--</option>
             <option   v-for="city in cities" :value="city.cityNameID">{{city.cityName}}</option>
@@ -11,9 +12,8 @@
           {{selectedCityID}}
         </div>
       </div>
-
-
     </div>
+
   </div>
 </template>
 

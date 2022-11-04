22bd5977510a7a37167d19c0667bbd559e55a150
Valeria <valeria.lukashevich@gmail.com>
2022-11-04 16:06:54 +0200

    commit

--- src/components/ServicesCheckbox.vue
+++ src/components/ServicesCheckbox.vue
@@ -1,28 +1,47 @@
 <template>
   <div>
-    <div class="form-check">
-      <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault">
+    <div v-for="service in atmServices" class="form-check">
+      <input v-model="service.isSelected"  class="form-check-input" type="checkbox" value="" id="flexCheckDefault">
       <label class="form-check-label" for="flexCheckDefault">
-        Default checkbox
+        {{service.serviceName}}
       </label>
     </div>
-    <div class="form-check">
-      <input class="form-check-input" type="checkbox" value="" id="flexCheckChecked" checked>
-      <label class="form-check-label" for="flexCheckChecked">
-        Checked checkbox
-      </label>
 
-    </div>
-    <div class="form-check">
-      <input class="form-check-input" type="checkbox" value="" id="flexCheckChecked" checked>
-      <label class="form-check-label" for="flexCheckChecked">
-        Checked checkbox
-      </label>
-    </div>
+    <button v-on:click="check()">Kontroll</button>
+
   </div>
 </template>
 <script>
 export default {
-  name: 'ServicesCheckbox'
+  name: 'ServicesCheckbox',
+  data: function () {
+    return {
+      atmServices: [
+        {
+          serviceId: 0,
+          serviceName: '',
+          isSelected: false
+        }
+      ]
+    }
+  },
+  methods: {
+    // check: function () {
+    //   alert('raha sisse ' + this.atmServices[0].isSelected + 'raha välja '+ this.atmServices[1].isSelected)
+    // },
+
+    getAtmServiceCheckboxInfo: function () {
+      this.$http.get('/atm/service')
+          .then(result => {
+            this.atmServices = result.data
+          }).catch(error => {
+            alert('noooooooo')
+      });
+    }
+
+  },
+  beforeMount() {
+    this.getAtmServiceCheckboxInfo()
+  }
 }
 </script>

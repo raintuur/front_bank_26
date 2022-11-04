7fe7ea6e1e2d79aa540a05af0c6e6f08385af1b1
HelenSo <helensoodla@gmail.com>
2022-11-04 12:47:35 +0200

    Fixed cities dropdown missing key, refactored CitiesDropdown.vue to separate component, added ServicesCheckbox.vue component (HTML only).

--- src/views/AtmView.vue
+++ src/views/AtmView.vue
@@ -10,21 +10,7 @@
 
 
           <div class="row">
-
-            <div class="form-check">
-              <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault">
-              <label class="form-check-label" for="flexCheckDefault">
-                Default checkbox
-              </label>
-            </div>
-            <div class="form-check">
-              <input class="form-check-input" type="checkbox" value="" id="flexCheckChecked" checked>
-              <label class="form-check-label" for="flexCheckChecked">
-                Checked checkbox
-              </label>
-
-            </div>
-
+            <ServicesCheckbox/>
 
 
 
@@ -40,10 +26,11 @@
 
 <script>
 import CitiesDropdown from "@/components/CitiesDropdown";
+import ServicesCheckbox from "@/components/ServicesCheckbox";
 
 export default {
   name: 'AtmView',
-  components: {CitiesDropdown},
+  components: {ServicesCheckbox, CitiesDropdown},
   data: function () {
     return {
 

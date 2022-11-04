4660da4538a34fb1a945af87fbc451f0658a39dc
Rain Tüür <rain.tuur@gmail.com>
2022-11-04 15:26:26 +0200

    Fixed cities dropdownd missing key, refactored CitiesDropdown to sepearate component, addded ServicesCheckbox component (HTML only).

--- src/components/ServicesCheckbox.vue
+++ src/components/ServicesCheckbox.vue
@@ -1,6 +1,7 @@
 <template>
   <div>
-    <div class="form-check">
+
+    <div v-for="? in ?" class="form-check">
       <input class="form-check-input" type="checkbox" id="flexCheckDefault">
       <label class="form-check-label" for="flexCheckDefault">
         Default checkbox

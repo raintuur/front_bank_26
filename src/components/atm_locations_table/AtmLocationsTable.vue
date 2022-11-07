b884efd982ffc553b19abb7b5d90ea56326838d7
Kristiina <kristiina.saarniit@yahoo.com>
2022-11-07 11:40:43 +0200

    Fixed cities dropdown missing key, refactored CitiesDropdown to separate component, added ServicesCheckbox component(HTML only)

---
+++ src/components/atm_locations_table/AtmLocationsTable.vue
@@ -0,0 +1,44 @@
+<template>
+  <table class="table table-dark table-hover">
+    <thead>
+    <tr>
+      <th scope="col">#</th>
+      <th scope="col">Linn</th>
+      <th scope="col">Asukoht</th>
+      <th scope="col">Teenused</th>
+      <th scope="col">nupp</th>
+    </tr>
+    </thead>
+    <tbody>
+
+
+    <!--  todo: tekitame ise järjekorra numbrid -->
+      <tr v-for="atmLocation in atmLocations">
+      <th scope="row">?</th>
+      <td>{{atmLocations.cityName}}</td>
+      <td>Otto</td>
+      <td>
+        <div class=" row">
+          ?????
+        </div>
+      </td>
+    <td>
+      <button type="button" class="btn btn-light">Light</button>
+    </td>
+    </tr>
+
+    </tbody>
+  </table>
+</template>
+
+<script>
+export default {
+  name: "AtmLocationsTable",
+  props: {
+    atmLocations: Array()
+  },
+  beforeMount() {
+    alert(JSON.stringify(this.atmLocations))
+  }
+}
+</script>

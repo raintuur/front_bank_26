915a8931e343b4f6e7774da40fa51f38b0ebde04
raili-git <raili.moim@gmail.com>
2022-11-07 11:37:59 +0200

    fixed cities drop-down missing key, refactored CitiesDropdown.vue to separated component, added ServicesCheckbox.vue component (HTML) only

---
+++ src/components/atm_locations_table/AtmLocationsTable.vue
@@ -0,0 +1,44 @@
+<template>
+  <table class="table table-dark table-hover">
+    <thead>
+    <tr>
+      <th scope="col">#</th>
+      <th scope="col">Linn</th>
+      <th scope="col">Asukoht</th>
+      <th scope="col">Teenused</th>
+      <th scope="col">Nupp</th>
+    </tr>
+    </thead>
+    <tbody>
+    <tr v-for="atmLocation in atmLocations">
+      <th scope="row">1</th>
+      <td>{{ atmLocation.cityName}}</td>
+      <td>Otto</td>
+      <td>
+        <div class="row">
+          ???
+        </div>
+      </td>
+      <td>
+        <button type="button" class="btn btn-light">Alert</button>
+      </td>
+    </tr>
+    </tbody>
+    <!-- todo: seda elementi hakkame for loopima objektist atmLocations'   -->
+    <!-- todo: eesmärk on saada see info propsist
+    ('atmLocations' objekt tuleb componenti kaasa anda)-->
+    <!-- todo: tekitame ise järjekorra numbrid   -->
+  </table>
+</template>
+
+<script>
+export default {
+  name: 'AtmLocationsTable',
+  props: {
+    atmLocations: []
+  },
+  beforeMount() {
+    alert(JSON.stringify(this.atmLocations))
+  }
+}
+</script>

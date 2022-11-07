14ca16e9f29193c8444c4ecc20587b76259dcd70
admin <vaher.kaupo@gmail.com>
2022-11-07 13:40:50 +0200

    Endpoint made

--- src/views/AtmView.vue
+++ src/views/AtmView.vue
@@ -4,18 +4,16 @@
     <div class="container">
       <div class="row justify-content-start">
         <div class="col col-lg-3">
-
-
           <CitiesDropdown/>
-
-
           <div class="row">
-
             <ServicesCheckbox/>
-
           </div>
-
         </div>
+
+        <div class="col col-lg-9" >
+          <AtmLocationsTable :atm-locations="atmLocations"/>
+        </div>
+
       </div>
     </div>
 
@@ -26,14 +24,53 @@
 <script>
 import CitiesDropdown from "@/components/CitiesDropdown";
 import ServicesCheckbox from "@/components/ServicesCheckbox";
+import AtmLocationsTable from "@/components/atm_locations_table/AtmLocationsTable";
 
 export default {
   name: 'AtmView',
-  components: {ServicesCheckbox, CitiesDropdown},
+  components: {ServicesCheckbox, CitiesDropdown, AtmLocationsTable},
   data: function () {
-    return {}
+    return {
+      atmLocations: [
+        {
+          cityName: '',
+          atmLocationInfo: '',
+          atmServices: [
+            {
+              serviceName: ''
+            }
+          ]
+        }
+      ],
+    }
   },
-  methods: {}
+  methods: {
+
+    getAllAtmLocations: function () {
+      this.$http.get("/atm/info")
+          .then(response => {
+
+            this.atmLocations = response.data
+            this,this.addSequenceNumbers();
+            console.log(response.data)
+          })
+          .catch(error => {
+            console.log(error)
+          })
+    },
+    addSequenceNumbers: function () {
+      let counter = 1
+      this.atmLocations.forEach(location => {
+        location.sequenceNumber = counter
+        counter++
+      })
+
+    }
+
+  },
+  beforeMount() {
+    this.getAllAtmLocations()
+  }
 }
 
 

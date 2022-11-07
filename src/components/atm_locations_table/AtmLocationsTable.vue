3d816a784ebd06692de7126c59d95be326cc0f88
temp <astrid.oras@gmail.com>
2022-11-07 11:29:24 +0200

    WIP

---
+++ src/components/atm_locations_table/AtmLocationsTable.vue
@@ -0,0 +1,49 @@
+<template>
+
+  <table class="table table-dark table-hover">
+    <thead>
+    <tr>
+      <th scope="col">#</th>
+      <th scope="col">Linn</th>
+      <th scope="col">Asukoht</th>
+      <th scope="col">Teenused</th>
+      <th scope="col">Nupp</th>
+    </tr>
+    </thead>
+    <tbody>
+    <!--    todo:seda elementi hakkame for loopima objektist 'atmLocations'     -->
+    <!--    todo:eesmärk on saada see info propsist ('atmLocations' objekt tuleb componenti kaasa anda   -->
+    <!--    todo:tekitame ise järjekorranumbrid   -->
+
+    <tr ">
+      <th scope="row">1</th>
+      <td>Mark</td>
+      <td>Otto</td>
+      <td>
+        <div class="row">
+          raha sisse
+        </div>
+      </td>
+      <td>
+        <button type="button" class="btn btn-light">Alert</button>
+      </td>
+    </tr>
+
+    </tbody>
+  </table>
+
+</template>
+
+<script>
+export default {
+  name: 'AtmLocationsTable',
+  props: {
+    atmLocations: {}
+  },
+  mounted() {
+
+    alert(JSON.stringify(this.atmLocations))
+  }
+}
+</script>
+
--- src/views/AtmView.vue
+++ src/views/AtmView.vue
@@ -4,18 +4,20 @@
     <div class="container">
       <div class="row justify-content-start">
         <div class="col col-lg-3">
-
-
           <CitiesDropdown/>
-
-
+          
           <div class="row">
 
             <ServicesCheckbox/>
 
           </div>
+        </div>
+
+        <div class="col col-9">
+          <AtmLocationsTable :atm-locations="atmLocations"/>
 
         </div>
+
       </div>
     </div>
 
@@ -26,14 +28,46 @@
 <script>
 import CitiesDropdown from "@/components/CitiesDropdown";
 import ServicesCheckbox from "@/components/ServicesCheckbox";
+import AtmLocationsTable from "@/components/atm_locations_table/AtmLocationsTable";
 
 export default {
   name: 'AtmView',
-  components: {ServicesCheckbox, CitiesDropdown},
+  components: {ServicesCheckbox, CitiesDropdown, AtmLocationsTable},
   data: function () {
-    return {}
+    return {
+      atmLocations: [
+        {
+          cityName:'',
+          atmLocationInfo: '',
+          atmServices: [
+            {
+              serviceName: ''
+            }
+          ]
+        }
+      ]
+      ,
+
+    }
   },
-  methods: {}
+  methods: {
+
+    getAllAtmLocations: function () {
+      this.$http.get("/atm/info")
+          .then(response => {
+
+            this.atmLocation=response.data
+
+            console.log(response.data)
+          })
+          .catch(error => {
+            console.log(error)
+          })
+    },
+  },
+  beforeMount() {
+    this.getAllAtmLocations()
+  }
 }
 
 

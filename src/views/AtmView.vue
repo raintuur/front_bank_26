f0d30cd2ea6dab86d4f7ed44cae2f442917cf2b8
Mario Mumm <mario.mumm@hotmail.com>
2022-11-07 12:26:09 +0200

    MARIO-FEB-3 Teenuste check boxid

--- src/views/AtmView.vue
+++ src/views/AtmView.vue
@@ -7,11 +7,14 @@
           <CitiesDropdown/>
 
           <div class="row">
-
             <ServicesCheckbox/>
-
           </div>
         </div>
+
+        <div class="col col-lg-9">
+          <AtmLocationsTable :atm-locations="atmLocations"/>
+        </div>
+
       </div>
     </div>
   </div>
@@ -20,14 +23,49 @@
 <script>
 import CitiesDropdown from "@/components/CitiesDropdown";
 import ServicesCheckbox from "@/components/ServicesCheckbox";
+import AtmLocationsTable from "@/components/atm_locations_table/AtmLocationsTable";
 
 export default {
   name: 'AtmView',
-  components: {ServicesCheckbox, CitiesDropdown},
+  components: {ServicesCheckbox, CitiesDropdown, AtmLocationsTable},
   data: function () {
-    return {}
-    methods: {
+    return {
+      atmLocations: [
+        {
+          cityName: '',
+          atmLocationInfo: '',
+          atmServices: [
+            {
+              serviceName: ''
+            }
+          ]
+        }
+      ],
+
     }
+  },
+  methods: {
+
+    getAllAtmLocations: function () {
+      this.$http.get("/atm/info")
+          .then(response => {
+            this.atmLocations = response.data
+            this.addSequenceNumbers()
+          })
+          .catch(error => {
+            console.log(error)
+          })
+    },
+    addSequenceNumbers: function () {
+      let counter = 1
+      this.atmLocations.forEach(location => {
+        location.sequenceNumber = counter
+        counter++
+      });
+    }
+  },
+  beforeMount() {
+    this.getAllAtmLocations()
   }
 }
 

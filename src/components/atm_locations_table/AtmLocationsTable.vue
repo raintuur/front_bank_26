1419f9f6d1dd45e576d19b5c8781eb1f29a85b2e
Andres Orgla <aorgla@gmail.com>
2022-11-07 11:39:10 +0200

    commit

---
+++ src/components/atm_locations_table/AtmLocationsTable.vue
@@ -0,0 +1,41 @@
+<template>
+  <table class="table table-dark table-hover">
+    <thead>
+    <tr>
+      <th scope="col"></th>
+      <th scope="col">Linn</th>
+      <th scope="col">Asukoht</th>
+      <th scope="col">Teenused</th>
+      <th scope="col">Nupp</th>
+    </tr>
+    </thead>
+    <tbody>
+
+    <!-- todo: tekitame ise järjekorra numbrid-->
+    <tr v-for="atmLocation in atmLocations">
+      <th scope="row">1</th>
+      <td>{{atmLocation.cityName}}</td>
+      <td>Otto</td>
+      <td>
+        <div class="row">
+          ??
+        </div>
+      </td>
+      <td>
+        <button type="button" class="btn btn-outline-light">Light</button>
+      </td>
+    </tr>
+    </tbody>
+  </table>
+</template>
+
+<script>
+export default {
+  name: "AtmLocationsTable",
+  props: {
+    atmLocations: {},
+  },
+  beforeMount() {
+  }
+}
+</script>
--- src/views/AtmView.vue
+++ src/views/AtmView.vue
@@ -4,15 +4,11 @@
     <div class="container">
       <div class="row justify-content-start">
         <div class="col col-sm-5">
-
           <CitiesDropdown/>
-
           <ServicesCheckBox/>
-
-<!--          tabel     -->
-
-
-
+        </div>
+        <div class="col col-sm-7">
+          <AtmLocationsTable :atm-locations="atmLocations"/>
         </div>
       </div>
     </div>
@@ -23,14 +19,45 @@
 <script>
 import CitiesDropdown from "@/components/CitiesDropdown";
 import ServicesCheckBox from "@/components/ServicesCheckBox";
+import AtmLocationsTable from "@/components/atm_locations_table/AtmLocationsTable";
 
 export default {
   name: 'AtmView',
-  components: {ServicesCheckBox, CitiesDropdown},
+  components: {ServicesCheckBox, CitiesDropdown, AtmLocationsTable},
   data: function () {
-    return {}
-  },
-  methods: {},
-}
+    return {
 
+      atmLocations: [
+        {
+          cityName: '',
+          atmLocationInfo: '',
+          atmServices: [
+            {
+              serviceName: ''
+            }
+          ]
+        }
+      ],
+
+    }
+  },
+  methods: {
+
+    getAllAtmLocations: function () {
+      this.$http.get("/atm/info")
+          .then(response => {
+
+            // this.atmLocations = response.data
+
+            console.log(response.data)
+          })
+          .catch(error => {
+            console.log(error)
+          })
+    },
+  },
+  beforeMount() {
+    this.getAllAtmLocations()
+  }
+}
 </script>

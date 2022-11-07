545b7cc55720667196f9c8792aca5a2bf06ec3ed
Mai-Triin <110418700+Mai-Triin@users.noreply.github.com>
2022-11-07 14:36:12 +0200

    MAI-FEB-4 Commit

---
+++ src/components/atm_locations_table/AtmLocationsTable.vue
@@ -0,0 +1,41 @@
+<template>
+  <table class="table table-dark table-striped">
+    <thead>
+    <tr>
+      <th scope="col">#</th>
+      <th scope="col">Linn</th>
+      <th scope="col">Asukoht</th>
+      <th scope="col">Teenused</th>
+      <th scope="col">nupp</th>
+    </tr>
+    </thead>
+    <tbody>
+
+    <!--  todo: seda elementi hakkame for loopima objektist 'atmLocations'  -->
+    <!--  todo: eesmärk on saada see info propsist ('atmLocations' objekt tuleb componenti kaasa anda) -->
+    <!--  todo: tekitame ise järjekorra numbrid  -->
+    <tr v-for="atmLocation in atmLocations" :key="atmLocation.atmLocationInfo">
+      <th scope="row">{{atmLocation.sequenceNumber}}</th>
+      <td>{{ atmLocation.cityName }}</td>
+      <td>{{ atmLocation.atmLocationInfo }}</td>
+      <td>
+        <div v-for="service in atmLocation.atmServices" class="row">
+          {{service.serviceName}}
+        </div>
+      </td>
+      <td>
+        <button type="button" class="btn btn-light">Alert</button>
+      </td>
+    </tr>
+    </tbody>
+  </table>
+</template>
+
+<script>
+export default {
+  name: 'AtmLocationsTable',
+  props: {
+    atmLocations: Array()
+  }
+}
+</script>
--- src/views/AtmView.vue
+++ src/views/AtmView.vue
@@ -4,18 +4,17 @@
     <div class="container">
       <div class="row justify-content-start">
         <div class="col col-lg-3">
-
           <CitiesDropdown/>
-
           <div class="row">
-
             <ServicesCheckbox/>
-
           </div>
-
         </div>
-      </div>
 
+        <div class="col col-lg-9">
+          <AtmLocationsTable :atm-locations="atmLocations" />
+        </div>
+
+      </div>
     </div>
   </div>
 </template>
@@ -23,14 +22,67 @@
 <script>
 import CitiesDropdown from "@/components/CitiesDropdown";
 import ServicesCheckbox from "@/components/ServicesCheckbox";
+import AtmLocationsTable from "@/components/atm_locations_table/AtmLocationsTable";
 
 export default {
   name: 'AtmView',
-  components: {ServicesCheckbox, CitiesDropdown},
+  components: {ServicesCheckbox, CitiesDropdown, AtmLocationsTable},
   data: function () {
-    return {}
+    return {
+      atmLocations: [
+        {
+          cityName: '',
+          atmLocationInfo: '',
+          atmServices: [
+            {
+              serviceName: ''
+            }
+          ]
+        }
+      ]
+    }
   },
-  methods: {}
+  methods: {
+
+    getAllAtmLocations: function () {
+      this.$http.get("/atm/info")
+          .then(response => {
+
+            this.atmLocations = response.data
+            this.addSequenceNumbers()
+            // console.log(response.data)
+          })
+          .catch(error => {
+            console.log(error)
+          })
+    },
+
+    getAtmLocationsById: function () {
+      this.$http.get("/atm/info/by-city", {
+            params: {
+              cityId: 15
+            }
+          }
+      ).then(response => {
+        console.log(response.data)
+      }).catch(error => {
+        console.log(error)
+      })
+    },
+
+    addSequenceNumbers: function () {
+      let counter = 1
+      this.atmLocations.forEach(location => {
+        location.sequenceNumber = counter
+        counter++
+      });
+    }
+
+  },
+  beforeMount() {
+    this.getAllAtmLocations()
+    this.getAtmLocationsById()
+  }
 }
 
 

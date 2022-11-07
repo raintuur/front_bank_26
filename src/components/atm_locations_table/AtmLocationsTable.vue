a3a6cae888e805d63daf18abc649b28aad1d8da8
admin <vaher.kaupo@gmail.com>
2022-11-07 15:44:12 +0200

    Endpoint made

--- src/components/atm_locations_table/AtmLocationsTable.vue
+++ src/components/atm_locations_table/AtmLocationsTable.vue
@@ -11,39 +11,30 @@
     </thead>
     <tbody>
 
-
-    <!--  todo: tekitame ise järjekorra numbrid -->
-      <tr v-for="atmLocation in atmLocations" :key="atmLocation.atmLocationInfo">
+    <!-- todo: tekitame ise järjekorra numbrid -->
+    <tr v-for="atmLocation in atmLocations" :key="atmLocation.atmLocationInfo">
       <th scope="row">{{atmLocation.sequenceNumber}}</th>
-
-      <td>{{atmLocation.cityName}}</td>
-      <td>{{atmLocation.atmLocationInfo}}</td>
+      <td>{{ atmLocation.cityName }}</td>
+      <td>{{ atmLocation.atmLocationInfo }}</td>
       <td>
-        <div v-for="service in atmLocation.atmServices" class=" row">
+        <div v-for="service in atmLocation.atmServices" class="row">
           {{service.serviceName}}
         </div>
       </td>
-    <td>
-      <button v-on:click="clickAlertButtonEvent(atmLocation.atmLocationInfo)" type="button" class="btn btn-light">Alert</button>
-    </td>
-    </tr>
+      <td>
+        <button type="button" class="btn btn-light">Alert</button>
+      </td>
 
+    </tr>
     </tbody>
   </table>
 </template>
 
 <script>
 export default {
-  name: "AtmLocationsTable",
+  name: 'AtmLocationsTable',
   props: {
     atmLocations: Array()
-  },
-
-  methods: {
-    clickAlertButtonEvent: function (locationName) {
-      this.$emit('clickAlertButtonEvent', locationName)
-
-    }
   }
 }
 </script>

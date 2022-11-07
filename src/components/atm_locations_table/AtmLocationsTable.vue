8f84cf296178dc9ea3d4cfa9ccb50bae9cd58f10
Valeria <valeria.lukashevich@gmail.com>
2022-11-07 15:44:27 +0200

    commit

---
+++ src/components/atm_locations_table/AtmLocationsTable.vue
@@ -0,0 +1,59 @@
+<template>
+  <table class="table table-dark table-hover">
+    <thead>
+    <tr>
+      <th scope="col">Nr.</th>
+      <th scope="col">Linn</th>
+      <th scope="col">Asukoht</th>
+      <th scope="col">Teenused</th>
+      <th scope="col">nupp</th>
+    </tr>
+    </thead>
+    <tbody>
+    <!--            todo: seda elementi hakkame for loopima objektist 'atmLocations'-->
+    <!--            todo: eesmärk on saada see info propsist -->
+    <!--            ('atmLocations' objekt tuleb komponenti kaasa anda)-->
+    <!--            todo: tekitame ise järjekorra numbrid-->
+    <tr v-for="atmLocation in atmLocations" :key="atmLocations.atmLocationInfo">
+      <th scope="row">{{atmLocation.sequenceNumber}}</th>
+      <td>{{ atmLocation.cityName }}</td>
+      <td>{{ atmLocation.atmLocationInfo }}</td>
+      <td>
+        <div v-for="service in atmLocation.atmServices" class="row">
+          {{ service.serviceName }}
+        </div>
+      </td>
+      <td>
+        <button v-on:click="clickAlertButtonEvent(atmLocation.atmLocationInfo)" type="button" class="btn btn-light">Alert</button>
+      </td>
+
+    </tr>
+    </tbody>
+  </table>
+</template>
+
+<script>
+export default {
+  name: "AtmLocationsTable",
+  props: {
+    atmLocations: Array()
+  },
+  mounted() {
+
+
+
+    let counter = 1
+    this.atmLocations.forEach(location => {
+      location.sequenceNumber = counter
+      counter++
+    })
+  },
+  methods: {
+    clickAlertButtonEvent: function (locationName) {
+      alert(locationName)
+      this.$emit('clickAlertButtonEvent',locationName)
+    }
+  }
+}
+</script>
+
--- src/views/AtmView.vue
+++ src/views/AtmView.vue
@@ -6,19 +6,19 @@
         <div class="col col-lg-3">
 
 
-          <CitiesDropdown/>
+          <CitiesDropdown @clickSelectCityEvent="getAllAtmLocationsById"/>
 
 
           <div class="row">
 
             <ServicesCheckbox/>
 
-
-
-
-
           </div>
 
+        </div>
+
+        <div class="col col-lg-9">
+          <AtmLocationsTable :atm-locations="atmLocations" @clickAlertButtonEvent="clickAlertButtonEvent"/>
         </div>
       </div>
     </div>
@@ -30,17 +30,72 @@
 <script>
 import CitiesDropdown from "@/components/CitiesDropdown";
 import ServicesCheckbox from "@/components/ServicesCheckbox";
+import AtmLocationsTable from "@/components/atm_locations_table/AtmLocationsTable";
 
 export default {
   name: 'AtmView',
-  components: {ServicesCheckbox, CitiesDropdown},
+  components: {ServicesCheckbox, CitiesDropdown, AtmLocationsTable},
   data: function () {
     return {
+      atmLocations: [
+        {
+          cityName: '',
+          atmLocationInfo: '',
+          atmServices: [
+            {
+              serviceName: ''
+            }
+          ]
+        }
+      ],
+
 
     }
   },
   methods: {
 
+    clickAlertButtonEvent: function (locationName) {
+      alert(locationName + 'alert from parent')
+
+    },
+    getAllAtmLocations: function () {
+      this.$http.get("/atm/info")
+          .then(response => {
+            this.atmLocations = response.data
+            this.addSequenceNumbers();
+
+          })
+          .catch(error => {
+            console.log(error)
+          })
+    },
+
+    getAllAtmLocationsById: function (selectedCityId) {
+      alert('cityId ' + selectedCityId)
+      this.$http.get("/atm/info/by-city", {
+            params: {
+              cityId: 15
+            }
+          }
+      ).then(response => {
+        this.atmLocations = response.data
+        console.log(response.data)
+      }).catch(error => {
+        console.log(error)
+      })
+    },
+
+
+    addSequenceNumbers: function () {
+      let counter = 1
+      this.atmLocations.forEach(location => {
+        location.sequenceNumber = counter
+        counter++
+      });
+    }
+  },
+  beforeMount() {
+    this.getAllAtmLocations()
   }
 }
 

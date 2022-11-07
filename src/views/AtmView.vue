e2eeb442c08725c62aadc21a2609bfb14a228e49
Rain Tüür <rain.tuur@gmail.com>
2022-11-07 10:31:18 +0200

    RAIN-FEB-4 WIP

--- src/views/AtmView.vue
+++ src/views/AtmView.vue
@@ -4,18 +4,12 @@
     <div class="container">
       <div class="row justify-content-start">
         <div class="col col-lg-3">
-
-
           <CitiesDropdown/>
-
-
           <div class="row">
-
             <ServicesCheckbox/>
-
           </div>
-
         </div>
+
       </div>
     </div>
 
@@ -31,9 +25,40 @@ export default {
   name: 'AtmView',
   components: {ServicesCheckbox, CitiesDropdown},
   data: function () {
-    return {}
+    return {
+      atmLocations: [
+        {
+          cityName: '',
+          atmLocationInfo: '',
+          atmServices: [
+            {
+              serviceName: ''
+            }
+          ]
+        }
+      ],
+
+
+    }
   },
-  methods: {}
+  methods: {
+
+    getAllAtmLocations: function () {
+      this.$http.get("/atm/info")
+          .then(response => {
+
+            this.atmLocations = response.data
+            console.log(response.data)
+          })
+          .catch(error => {
+            console.log(error)
+          })
+    },
+
+  },
+  beforeMount() {
+    this.getAllAtmLocations()
+  }
 }
 
 

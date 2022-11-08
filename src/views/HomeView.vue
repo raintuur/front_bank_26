d88a5f9f9ebeeb2a15525bff9e3337cbc9abda74
Rain Tüür <rain.tuur@gmail.com>
2022-11-08 14:19:30 +0200

    RAIN-FEB-4 WIP

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,31 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="HELLO WORLD"/>
+    <div class="row">
+      <h1>? Meie pank on kõige lahedam!!!</h1>
+    </div>
+
+    <div class="row">
+
+      <div class="col-lg-4">
+        <h2>See on meie eriti äge pank</h2>
+        <p>Siin saab mugavalt vaadata oma kontojääki, vaadata kontoväljavõtet,
+          teha makseid jne.</p>
+      </div>
+
+      <div class="col-lg-6">
+        <h3>Logi sisse</h3>
+
+      </div>
+
+    </div>
+
+
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
 
 export default {
-  name: 'HomeView',
-  components: {
-    HelloWorld
-  }
+  name: 'HomeView'
 }
 </script>

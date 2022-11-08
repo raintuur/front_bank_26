6f74ea451e32cf2a106a54266bbb5a00ab4006f0
Mairold <mairold.maegi@gmail.com>
2022-11-08 15:27:15 +0200

    MAIROLD-FEB-6 WIP

--- src/components/home/Login.vue
+++ src/components/home/Login.vue
@@ -1,27 +1,77 @@
 <template>
-  <div class="row row-cols-1 justify-content-center m-5">
+  <div class="row row-cols-1 justify-content-center">
     <div class="col">
       <h3>Sisselogimine</h3>
     </div>
     <div class="col col-sm-5">
-      <div class="input-group mb-3 ">
-        <span class="input-group-text" id="inputGroup-sizing-default">Kasutajanimi</span>
-        <input type="text" class="form-control" aria-label="Sizing example input"
-               aria-describedby="inputGroup-sizing-default">
-      </div>
+      <LoginError :message="message"/>
     </div>
+
     <div class="w-100"></div>
+
     <div class="col col-sm-5">
-      <div class="input-group mb-3 ">
-        <span class="input-group-text" id="inputGroup-sizing-default2">Parool</span>
-        <input type="text" class="form-control" aria-label="Sizing example input"
-               aria-describedby="inputGroup-sizing-default2">
+      <div class="input-group mb-3">
+        <span class="input-group-text" id="basic-addon1">Kasutajanimi</span>
+        <input v-model="username" type="text" class="form-control" placeholder="kasutajanimi" aria-label="Username"
+               aria-describedby="basic-addon1">
       </div>
     </div>
+
+    <div class="w-100"></div>
+
+    <div class="col col-sm-5">
+      <div class="input-group mb-3">
+        <span class="input-group-text" id="basic-addon2">Parool</span>
+        <input v-model="password" type="password" class="form-control" placeholder="parool" aria-label="Username"
+               aria-describedby="basic-addon2">
+      </div>
+    </div>
+
+    <div class="w-100"></div>
+
+    <div class="col col-sm-5">
+      <button v-on:click="login" type="button" class="btn btn-primary">Logi sisse</button>
+    </div>
+
   </div>
 </template>
 <script>
+import LoginError from "@/components/home/LoginError";
+
 export default {
-  name: 'Login'
+  name: 'Login',
+  components: {LoginError},
+  data: function () {
+    return {
+      username: '',
+      password: '',
+      message: ''
+    }
+  },
+
+  methods: {
+    login: function () {
+      if(this.username.length <= 0 || this.password.length <= 0) {
+        this.message = 'Täida kõik väljad!'
+      } else {
+        this.message = ''
+        this.$http.get("/some/path", {
+              params: {
+                username: this.username,
+                password: this.password
+              }
+            }
+        ).then(response => {
+          console.log(response.data)
+        }).catch(error => {
+          console.log(error)
+        })
+      }
+    },
+
+
+
+  },
+
 }
 </script>

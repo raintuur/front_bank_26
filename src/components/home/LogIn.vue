18b99d9d8598ffe3136136569080cbf07aa89af9
Rain Tüür <rain.tuur@gmail.com>
2022-11-08 15:27:04 +0200

    RAIN-FEB-6 WIP

--- src/components/home/LogIn.vue
+++ src/components/home/LogIn.vue
@@ -6,9 +6,12 @@
 
       <div class="col-lg-5">
 
+        <AlertError :message="message"/>
+
+
         <div class="input-group mb-3">
           <span class="input-group-text">Kasutajanimi</span>
-          <input v type="text" class="form-control" >
+          <input v-model="username" type="text" class="form-control">
         </div>
 
         <div class="input-group mb-3">
@@ -17,7 +20,7 @@
         </div>
 
         <div class="d-grid gap-2 col-6 mx-auto">
-          <button class="btn btn-primary" type="button">Logi sisse</button>
+          <button v-on:click="login" class="btn btn-primary" type="button">Logi sisse</button>
         </div>
 
       </div>
@@ -29,13 +32,41 @@
   </div>
 </template>
 <script>
+import AlertError from "@/components/alert/AlertError";
+
 export default {
   name: 'LogIn',
+  components: {AlertError},
   data: function () {
     return {
       username: '',
       password: '',
+      message: ''
     }
+  },
+  methods: {
+    login: function () {
+
+      this.message = ''
+      if (this.username.length == 0 || this.password.length == 0) {
+        this.message = 'Täida kõik väljad'
+      } else {
+        // ei ole täidetud
+        this.$http.get("/some/path", {
+              params: {
+                someRequestParam1: this.someDataBlockVariable1,
+                someRequestParam2: this.someDataBlockVariable2
+              }
+            }
+        ).then(response => {
+          console.log(response.data)
+        }).catch(error => {
+          console.log(error)
+        });
+      }
+
+
+    },
   }
 }
 </script>

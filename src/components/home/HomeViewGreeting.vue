5e24f67f8745207ae53e3d53c688efccdf6a4623
Anton Klink <anton.klink@gmail.com>
2022-11-08 14:34:51 +0200

    123

---
+++ src/components/home/HomeViewGreeting.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <div class="row justify-content-center">
+      <h2>See on meie eriti äge pank</h2>
+      <p>Siin saab mugavalt vaadata oma kontojääki, vaadata kontoväljavõtet,
+        teha makseid jne.</p>
+    </div>
+  </div>
+
+</template>
+<script>
+export default {
+  name: 'HomeViewGreeting'
+}
+</script>
---
+++ src/components/home/LogIn.vue
@@ -0,0 +1,21 @@
+<template>
+
+  <div>
+
+    <div class="row justify-content-center">
+      <h3>Logi sisse</h3>
+
+      <div class="col-lg-3">
+
+        <input>
+
+      </div>
+    </div>
+  </div>
+
+</template>
+<script>
+export default {
+  name: 'LogIn'
+}
+</script>

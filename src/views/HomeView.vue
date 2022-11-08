e87caa486f8dcfbdb0598b52a7bea62323529d77
Liis R <liisvoks@gmail.com>
2022-11-08 14:22:21 +0200

    LIIS-FEB-5

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,7 +1,18 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="HELLO WORLD"/>
+    <div class="row">
+      <h1> See on meie eriti äge pank </h1>
+    </div>
+    <div class="row">
+      <div class="col-lg-6">
+        <h2> See on meie eriti äge pank </h2>
+      </div>
+
+      <div class="col-lg-3">
+        <h3> Sisseloimine </h3>
+      </div>
+    </div>
+
   </div>
 </template>
 
@@ -11,8 +22,6 @@ import HelloWorld from '@/components/HelloWorld.vue'
 
 export default {
   name: 'HomeView',
-  components: {
-    HelloWorld
-  }
+  components: {}
 }
 </script>

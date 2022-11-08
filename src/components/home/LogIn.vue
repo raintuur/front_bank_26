69178630dec6af46a3f172edd86ea27526cac990
Anton Klink <anton.klink@gmail.com>
2022-11-08 15:27:18 +0200

    123

--- src/components/home/LogIn.vue
+++ src/components/home/LogIn.vue
@@ -1,21 +1,69 @@
 <template>
   <div>
-    <h3>Logi sisse</h3>
+    <h3>Sisselogimine</h3>
 
     <div class="row justify-content-center">
+      <div class="col-lg-5">
 
-      <div class="col-lg-3">
-        <input>
+        <AlertError :message="message"/>
+
+        <div class="input-group mb-3">
+          <span class="input-group-text">Kasutajanimi</span>
+          <input v-model="username" type="text" class="form-control">
+        </div>
+        <div class="input-group mb-3">
+          <span class="input-group-text">Parool</span>
+          <input v-model="password" type="password" class="form-control">
+        </div>
       </div>
-
-
     </div>
 
+    <div class="d-grid gap-2 col-2 mx-auto">
+      <button v-on:click="login" class="btn btn-primary" type="button">Logi sisse</button>
+    </div>
 
   </div>
 </template>
 <script>
+import AlertError from "@/components/alert/AlertError";
+
 export default {
-  name: 'LogIn'
+  name: 'LogIn',
+  components: {AlertError},
+  data: function () {
+    return {
+      username: '',
+      password: '',
+      message: 'TÄIDA UUESTI!'
+    }
+  },
+
+  // live template vhGetParam
+
+  methods: {
+    login: function () {
+
+
+      if (this.username.length == 0 || this.password.length == 0) {
+
+        this.message = 'Täida kõik väljad'
+
+      } else {
+
+        this.$http.get("/some/path", {
+              params: {
+                someRequestParam1: this.someDataBlockVariable1,
+                someRequestParam2: this.someDataBlockVariable2
+              }
+            }
+        ).then(response => {
+          console.log(response.data)
+        }).catch(error => {
+          console.log(error)
+        });
+      }
+    }
+  },
 }
+
 </script>

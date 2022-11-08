60634e78ded0178deae1f6afc5297ca6e658ab27
Mihke <mihkelviirsalu@hotmail.com>
2022-11-08 14:33:24 +0200

    MIHKEL-FEB-6 WIP

---
+++ src/components/home/HomeViewGreeting.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>
+    <h2>See on meie eriti äge pank</h2>
+    <p> Siin saab toiminguid teha</p>
+  </div>
+
+</template>
+<script>
+export default {
+  name: 'HomeViewGreeting'
+}
+</script>
---
+++ src/components/home/LogIn.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <h3>Sisselogimine</h3>
+    
+    <div class="row justify-content-center">
+      <div class="col-lg-3">
+        <input type="text">
+      </div>
+    </div>
+  
+  </div>
+</template>
+<script>
+export default {
+  name: 'LogIn'
+}
+</script>

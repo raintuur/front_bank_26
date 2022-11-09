4635dc2f83b8efcedb111808caa95febf20abd14
Mai-Triin <110418700+Mai-Triin@users.noreply.github.com>
2022-11-09 14:51:46 +0200

    MAI-FEB-6 Commit

---
+++ src/components/home/HomeViewGreeting.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>
+    <h2>See on meie eriti äge pank</h2>
+    <p>Siin saab mugavalt vaadata oma kontojääki, vaadata kontoväljavõtet, teha makseid jne</p>
+  </div>
+</template>
+<script>
+export default {
+  name: 'HomeViewGreeting'
+}
+</script>
--- src/router/index.js
+++ src/router/index.js
@@ -4,44 +4,56 @@ import HomeView from '../views/HomeView.vue'
 import AtmView from "@/views/AtmView";
 import CustomersView from "@/views/CustomersView";
 import CarsView from "@/views/CarsView";
+import CustomerHomeView from "@/views/CustomerHomeView";
+import AdminHomeView from "@/views/AdminHomeView";
 
 Vue.use(VueRouter)
 
 const routes = [
-  {
-    path: '/',
-    name: 'home',
-    component: HomeView
-  },
-  {
-    path: '/atm',
-    name: 'atmRoute',
-    component: AtmView
-  },
-  {
-    path: '/customer',
-    name: 'customerRoute',
-    component: CustomersView
-  },
-  {
-    path: '/car',
-    name: 'carRoute',
-    component: CarsView
-  },
-  {
-    path: '/about',
-    name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: function () {
-      return import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
+    {
+        path: '/',
+        name: 'home',
+        component: HomeView
+    },
+    {
+        path: '/customer/home',
+        name: 'customerHomeRoute',
+        component: CustomerHomeView
+    },
+    {
+        path: '/admin/home',
+        name: 'adminHomeRoute',
+        component: AdminHomeView
+    },
+    {
+        path: '/atm',
+        name: 'atmRoute',
+        component: AtmView
+    },
+    {
+        path: '/customer',
+        name: 'customerRoute',
+        component: CustomersView
+    },
+    {
+        path: '/car',
+        name: 'carRoute',
+        component: CarsView
+    },
+    {
+        path: '/about',
+        name: 'about',
+        // route level code-splitting
+        // this generates a separate chunk (about.[hash].js) for this route
+        // which is lazy-loaded when the route is visited.
+        component: function () {
+            return import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
+        }
     }
-  }
 ]
 
 const router = new VueRouter({
-  routes
+    routes
 })
 
 export default router

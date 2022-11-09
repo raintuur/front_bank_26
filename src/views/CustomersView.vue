b24ca182ccac120326294281b3de14795e649a59
Kristiina <kristiina.saarniit@yahoo.com>
2022-11-09 21:03:19 +0200

    Muudkui testin

--- src/views/CustomersView.vue
+++ src/views/CustomersView.vue
@@ -1,19 +1,12 @@
 <template>
-  <div>
-    <div class="row justify-content-md-center m-5">
-      <div class="col-lg-10">
-        <CustomersTable/>
-      </div>
-    </div>
-  </div>
+
+
 </template>
 
 <script>
-import CustomersTable from "@/components/customer_table/CustomersTable";
 
 export default {
-  name: "CustomersView",
-  components: {CustomersTable}
+  name: "CustomersView"
 }
 </script>
 

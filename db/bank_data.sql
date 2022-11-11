95cc20cf2a9441fc5c81612b775dc8a13f3c1be5
Rain Tüür <rain.tuur@gmail.com>
2022-11-11 10:51:29 +0200

    VERTABELO

--- db/bank_data.sql
+++ db/bank_data.sql
@@ -1,5 +1,16 @@
-INSERT INTO public.atm (id, serial_number) VALUES (DEFAULT, 'AAA123');
-INSERT INTO public.atm (id, serial_number) VALUES (DEFAULT, 'BBB123');
+INSERT INTO public.city (id, name) VALUES (DEFAULT, 'Tallinn');
+INSERT INTO public.city (id, name) VALUES (DEFAULT, 'Tartu');
+INSERT INTO public.city (id, name) VALUES (DEFAULT, 'Pärnu');
+INSERT INTO public.city (id, name) VALUES (DEFAULT, 'Viljandi');
+
+INSERT INTO public.location (id, city_id, name, address, longitude, latitude) VALUES (DEFAULT, 1, 'Tondi Selver', 'Tondi 1', null, null);
+INSERT INTO public.location (id, city_id, name, address, longitude, latitude) VALUES (DEFAULT, 1, 'Järve Selver', 'Järve 22', null, null);
+INSERT INTO public.location (id, city_id, name, address, longitude, latitude) VALUES (DEFAULT, 2, 'Veeriku Selver', 'Veeriku 7', null, null);
+INSERT INTO public.location (id, city_id, name, address, longitude, latitude) VALUES (DEFAULT, 3, 'Port Arturi Selver', 'Artur Rinne 2', null, null);
+
+
+INSERT INTO public.atm      (id, serial_number, location_id) VALUES   (DEFAULT, 'AAA123', 4);
+INSERT INTO public.atm (id, serial_number) VALUES (DEFAULT, 'BBB123',);
 INSERT INTO public.atm (id, serial_number) VALUES (DEFAULT, 'CCC123');
 
 INSERT INTO public.atm_service (id, name) VALUES (DEFAULT, 'raha välja');

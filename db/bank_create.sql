c69f73f93090cc0963347b9fa6ea0ddafdbc0f06
Anton Klink <anton.klink@gmail.com>
2022-11-11 10:57:13 +0200

    123

--- db/bank_create.sql
+++ db/bank_create.sql
@@ -1,7 +1,53 @@
+-- Kustutab public schema (mis põhimõtteliselt kustutab kõik tabelid)
+DROP SCHEMA public CASCADE;
+-- Loob uue public schema vajalikud õigused
+CREATE SCHEMA public
+-- taastab vajalikud andmebaasi õigused
+    GRANT ALL ON SCHEMA public TO postgres;
+GRANT ALL ON SCHEMA public TO PUBLIC;
 -- Created by Vertabelo (http://vertabelo.com)
--- Last modification date: 2022-11-10 12:40:24.742
+-- Last modification date: 2022-09-16 09:58:27.985
+
+
+-- Created by Vertabelo (http://vertabelo.com)
+-- Last modification date: 2022-11-11 08:02:33.847
 
 -- tables
+-- Table: atm
+CREATE TABLE atm
+(
+    id            serial       NOT NULL,
+    serial_number varchar(255) NOT NULL,
+    location_id   int          NOT NULL,
+    CONSTRAINT atm_pk PRIMARY KEY (id)
+);
+
+-- Table: atm_service
+CREATE TABLE atm_service
+(
+    id   serial       NOT NULL,
+    name varchar(255) NOT NULL,
+    CONSTRAINT atm_service_pk PRIMARY KEY (id)
+);
+
+-- Table: atm_service_relation
+CREATE TABLE atm_service_relation
+(
+    id             serial NOT NULL,
+    atm_id         int    NOT NULL,
+    atm_service_id int    NOT NULL,
+    CONSTRAINT atm_service_relation_pk PRIMARY KEY (id)
+);
+
+-- Table: city
+CREATE TABLE city
+(
+    id   serial       NOT NULL,
+    name varchar(255) NOT NULL,
+    CONSTRAINT city_ak_1 UNIQUE (name) NOT DEFERRABLE INITIALLY IMMEDIATE,
+    CONSTRAINT city_pk PRIMARY KEY (id)
+);
+
 -- Table: customer
 CREATE TABLE customer
 (
@@ -14,5 +60,53 @@ CREATE TABLE customer
     CONSTRAINT customer_pk PRIMARY KEY (id)
 );
 
--- End of file.
+-- Table: location
+CREATE TABLE location
+(
+    id        serial        NOT NULL,
+    city_id   int           NOT NULL,
+    name      varchar(255)  NOT NULL,
+    address   varchar(255)  NOT NULL,
+    longitude decimal(6, 2) NULL,
+    latitude  decimal(6, 2) NULL,
+    CONSTRAINT location_pk PRIMARY KEY (id)
+);
 
+-- foreign keys
+-- Reference: atm_location (table: atm)
+ALTER TABLE atm
+    ADD CONSTRAINT atm_location
+        FOREIGN KEY (location_id)
+            REFERENCES location (id)
+            NOT DEFERRABLE
+                INITIALLY IMMEDIATE
+;
+
+-- Reference: atm_service_relation_atm (table: atm_service_relation)
+ALTER TABLE atm_service_relation
+    ADD CONSTRAINT atm_service_relation_atm
+        FOREIGN KEY (atm_id)
+            REFERENCES atm (id)
+            NOT DEFERRABLE
+                INITIALLY IMMEDIATE
+;
+
+-- Reference: atm_service_relation_atm_service (table: atm_service_relation)
+ALTER TABLE atm_service_relation
+    ADD CONSTRAINT atm_service_relation_atm_service
+        FOREIGN KEY (atm_service_id)
+            REFERENCES atm_service (id)
+            NOT DEFERRABLE
+                INITIALLY IMMEDIATE
+;
+
+-- Reference: location_city (table: location)
+ALTER TABLE location
+    ADD CONSTRAINT location_city
+        FOREIGN KEY (city_id)
+            REFERENCES city (id)
+            NOT DEFERRABLE
+                INITIALLY IMMEDIATE
+;
+
+-- End of file.

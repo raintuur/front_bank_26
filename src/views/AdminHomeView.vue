5961b07d05c28e1cf7bab1f98e4b6e27479a57d9
Mario Mumm <mario.mumm@hotmail.com>
2022-11-21 16:30:46 +0200

    VERTABELO

--- src/views/AdminHomeView.vue
+++ src/views/AdminHomeView.vue
@@ -2,16 +2,77 @@
   <div>
     <h1>Admin koduvaade</h1>
     <h3>Adminni id: {{ adminId }}</h3>
+
+
+    <div class="d-grid gap-2 col-3 mx-auto">
+
+      <div class="input-group mb-3">
+        <span class="input-group-text" id="inputGroup-sizing-default">ATM seeria number</span>
+        <input v-model="atmRequest.serialNumber" type="text" placeholder = "AAA" class="form-control" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-default">
+      </div>
+
+      <select v-model="atmRequest.status" class="form-select" aria-label="Default select example">
+        <option selected disabled value="X">Vali staatus</option>
+        <option value="A">aktiivne</option>
+        <option value="P">aktiveerimata</option>
+      </select>
+
+      <ServicesCheckbox :atm-options="atmRequest.options"/>
+
+      <button v-on:click="addAtm" class="btn btn-primary" type="button">Lisa ATM</button>
+
+    </div>
+
   </div>
 </template>
 
 <script>
+import ServicesCheckbox from "@/components/ServicesCheckbox";
+
 export default {
   name: 'AdminHomeView',
+  components: {ServicesCheckbox},
   data: function () {
     return {
-      adminId: sessionStorage.getItem('userId')
+      adminId: sessionStorage.getItem('userId'),
+      atmRequest: {
+        locationId: sessionStorage.getItem('locationId'),
+        serialNumber: '',
+        status: 'X',
+        options: [
+          {
+            optionId: 0,
+            optionName: '',
+            isSelected: true
+          }
+        ]
+      }
     }
+  },
+  methods: {
+
+    addAtm: function () {
+      this.$http.post("/atm/new", this.atmRequest
+      ).then(response => {
+        console.log(response.data)
+      }).catch(error => {
+        console.log(error)
+      })
+    },
+
+
+    getAtmServicesCheckboxInfo: function () {
+      this.$http.get('/atm/option')
+          .then(result => {
+            this.atmRequest.options = result.data
+          })
+          .catch(error => {
+            alert("NO!!!!")
+          });
+    },
+  },
+  beforeMount() {
+    this.getAtmServicesCheckboxInfo()
   }
 }
 </script>

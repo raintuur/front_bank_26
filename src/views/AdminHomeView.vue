9f2993ff5cc53fed644a15ccb72579a52cc66659
Kristiina <kristiina.saarniit@yahoo.com>
2022-11-21 14:55:38 +0200

    VERTABELO

--- src/views/AdminHomeView.vue
+++ src/views/AdminHomeView.vue
@@ -8,11 +8,11 @@
 
       <div class="input-group mb-3">
         <span class="input-group-text" id="inputGroup-sizing-default">ATM seeria number</span>
-        <input type="text" placeholder="AAA" class="form-control" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-default">
+        <input v-model="atmRequest.serialNumber" type="text" placeholder="AAA" class="form-control" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-default">
       </div>
 
-      <select class="form-select" aria-label="Default select example">
-        <option selected disabled >Vali staatus</option>
+      <select v-model="atmRequest.status" class="form-select" aria-label="Default select example">
+        <option selected disabled value="X" >Vali staatus</option>
         <option value="A">aktiivne</option>
         <option value="P">aktiveerimata</option>
       </select>
@@ -20,7 +20,7 @@
 
       <ServicesCheckbox :atm-options="atmRequest.options"/>
 
-      <button class="btn btn-primary" type="button">Lisa ATM</button>
+      <button v-on:click="addAtm" class="btn btn-primary" type="button">Lisa ATM</button>
 
     </div>
 
@@ -39,7 +39,7 @@ export default {
       atmRequest: {
         locationId: sessionStorage.getItem('locationId'),
         serialNumber: '',
-        status: '',
+        status: 'X',
         options: [
           {
             optionId: 0,
@@ -51,6 +51,17 @@ export default {
     }
   },
   methods: {
+
+    addAtm: function () {
+      this.$http.post("/atm/new", this.atmRequest
+      ).then(response => {
+        console.log(response.data)
+      }).catch(error => {
+        console.log(error)
+      })
+    },
+
+
     getAtmServicesCheckboxInfo: function () {
       this.$http.get('/atm/option')
           .then(result => {

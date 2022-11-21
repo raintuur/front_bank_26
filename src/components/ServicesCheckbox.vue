7bd70d0fc05ca4903058d103814c6bea3c35b469
Rain Tüür <rain.tuur@gmail.com>
2022-11-21 14:43:25 +0200

    VERTABELO

--- src/components/ServicesCheckbox.vue
+++ src/components/ServicesCheckbox.vue
@@ -1,40 +1,18 @@
 <template>
   <div>
-    <div v-for="service in atmOptions" class="form-check">
-      <input v-model="service.isSelected" class="form-check-input" type="checkbox" id="flexCheckDefault">
+    <div v-for="option in atmOptions" class="form-check">
+      <input v-model="option.isSelected" class="form-check-input" type="checkbox" id="flexCheckDefault">
       <label class="form-check-label" for="flexCheckDefault">
-        {{ service.optionName }}
+        {{ option.optionName }}
       </label>
     </div>
   </div>
 </template>
 <script>
 export default {
-  name: 'ServicesCheckbox',
-  data: function () {
-    return {
-      atmOptions: [
-        {
-          optionId: 0,
-          optionName: '',
-          isSelected: false
-        }
-      ]
-    }
+  props: {
+    atmOptions: {}
   },
-  methods: {
-    getAtmServicesCheckboxInfo: function () {
-      this.$http.get('/atm/option')
-          .then(result => {
-            this.atmOptions = result.data
-          })
-          .catch(error => {
-            alert("NO!!!!")
-          });
-    }
-  },
-  beforeMount() {
-    this.getAtmServicesCheckboxInfo()
-  }
+  name: 'ServicesCheckbox'
 }
 </script>

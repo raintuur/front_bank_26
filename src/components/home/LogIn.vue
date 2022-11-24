6ae29bacc6d03f15144e02650755a364ee5b6c38
Rain Tüür <rain.tuur@gmail.com>
2022-11-24 10:59:15 +0200

    VERTABELO

--- src/components/home/LogIn.vue
+++ src/components/home/LogIn.vue
@@ -6,7 +6,7 @@
 
       <div class="col-lg-5">
 
-        <AlertError :message="errorMessage"/>
+        <AlertError :message="errorResponse.message"/>
 
 
         <div class="input-group mb-3">
@@ -41,71 +41,80 @@ export default {
     return {
       username: '',
       password: '',
-      errorMessage: '',
-      loginInfo: {
+
+      loginResponse: {
         userId: '',
         roleId: 0,
         roleType: 0,
-      }
+      },
+      errorResponse: {
+        message: '',
+        errorCode: 0
+      },
     }
   },
   methods: {
+
     login: function () {
-      this.errorMessage = ''
-
+      this.errorResponse.message = ''
       if (this.username.length == 0 || this.password.length == 0) {
-        this.errorMessage = 'Täida kõik väljad'
+        this.displayRequiredFieldsNotFilledAlert();
       } else {
-
-        // Stoplighti näide, et kuidas sundida mock serverti mingit kindlat vastust andma
-        // let preference = ''
-        // switch (this.username) {
-        //   case 'admin':
-        //     preference = 'code=200, example=200 - admin'
-        //     break;
-        //   case 'multirole':
-        //     preference = 'code=200, example=200 - multirole'
-        //     break;
-        //   case 'customer':
-        //     preference = 'code=200, example=200 - customer'
-        //     break;
-        // }
-
-        this.$http.get("/login", {
-              // Stoplighti näide, et kuidas sundida mock serverti mingit kindlat vastust andma
-              // headers: {
-              //   'Content-Type': 'application/json',
-              //   Prefer: preference
-              // },
-              params: {
-                username: this.username,
-                password: this.password
-              }
-            }
-        ).then(response => {
-          console.log(response.data)
-          this.loginInfo = response.data
-
-          sessionStorage.setItem('userId', this.loginInfo.userId)
-          this.$router.push({name: 'adminHomeRoute'})
-
-          this.$router.push({
-            name: 'customerHomeRoute', query: {
-              userId: this.loginInfo.userId,
-              roleName: this.loginInfo.roles[0].roleName
-            }
-          })
-
-
-
-
-        }).catch(error => {
-          console.log(error)
-        });
+        this.sendLoginRequest();
       }
-
-
     },
+
+    displayRequiredFieldsNotFilledAlert: function () {
+      this.errorResponse.message = 'Täida kõik väljad';
+    },
+
+    sendLoginRequest: function () {
+      this.$http.get("/login", {
+            params: {
+              username: this.username,
+              password: this.password
+            }
+          }
+      ).then(response => {
+        this.loginResponse = response.data
+        this.navigateToNextPage();
+      }).catch(error => {
+        this.errorResponse = error.response.data
+      });
+    },
+
+
+    navigateToNextPage: function () {
+      if (this.loginResponse.roleType === 'admin') {
+        this.navigateToAdmin();
+      } else {
+        this.navigateToCustomer();
+      }
+    },
+
+    navigateToAdmin: function () {
+      sessionStorage.setItem('userId', this.loginResponse.userId);
+      this.$router.push({
+        name: 'adminHomeRoute'
+      });
+    },
+
+    navigateToCustomer: function () {
+      this.$router.push({
+        name: 'customerHomeRoute', query: {
+          userId: this.loginResponse.userId,
+          roleName: this.loginResponse.roleType
+        }
+      })
+    },
+
+
+
   }
 }
 </script>
+
+
+
+
+

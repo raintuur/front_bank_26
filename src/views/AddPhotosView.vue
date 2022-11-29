8b6058cf27dd19733ce9b98fd9c3a0475ddf0d8a
temp <astrid.oras@gmail.com>
2022-11-29 15:06:58 +0200

    Image

---
+++ src/views/AddPhotosView.vue
@@ -0,0 +1,76 @@
+<template>
+  <div>
+    <ImageInput @pictureInputSuccess="setPicture"/>
+
+    <button v-on:click="addPicture" class="btn btn-primary" type="button">Salvesta pilt</button>
+
+    <div class="row">
+      <!--      todo: kui pictureData == null-->
+      <div v-if="pictureResponse.pictureData=== 0">
+        <img src="../assets/defaultkuusk.png" class="myPicSize">
+      </div>
+      <div v-else>
+        <img :src="pictureResponse.pictureData" class="myPicSize">
+      </div>
+
+    </div>
+  </div>
+
+</template>
+
+<script>
+import ImageInput from "@/components/image/ImageInput";
+
+export default {
+  name: "AddPhotosView",
+  components: {ImageInput},
+  data: function () {
+    return {
+      userId: sessionStorage.getItem('userId'),
+      pictureRequest: {
+        userId: 0,
+        pictureData: ''
+      },
+      pictureResponse: {
+        userId: 0,
+        pictureData: ''
+      }
+    }
+  },
+  methods: {
+    setPicture: function (picture) {
+      this.pictureRequest.pictureData = picture;
+    },
+
+    addPicture: function () {
+      this.pictureRequest.userId =this.userId
+      this.$http.post("/photo", this.pictureRequest
+      ).then(response => {
+       this.getUserPhoto()
+      }).catch(error => {
+        console.log(error)
+      })
+    },
+
+    getUserPhoto: function () {
+      this.$http.get("/photo", {
+            params: {
+              userId: this.userId
+            }
+          }
+      ).then(response => {
+        this.pictureResponse = response.data
+        console.log(response.data)
+      }).catch(error => {
+        console.log(error)
+      })
+    },
+
+  },
+
+  beforeMount() {
+    this.getUserPhoto()
+  }
+}
+</script>
+

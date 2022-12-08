2dcba9ec00c0e22017417d93a22cc1bfa768b4b6
Mario Mumm <mario.mumm@hotmail.com>
2022-12-08 11:22:10 +0200

    VERTABELO

--- src/components/home/HomeviewGreeting.vue
+++ src/components/home/HomeviewGreeting.vue
@@ -1,11 +1,12 @@
 <template>
-  <div>
-    <h2>See on meie eriti äge pank</h2>
-    <p>Siin saab mugavalt vaadata oma kontojääki, vaadata kontoväljavõtet, teha makseid</p>
-  </div>
+<div>
+  <h2>See on meie eriti äge pank</h2>
+  <p>Siin saab mugavalt vaadata oma kontojääki, vaadata kontoväljavõtet,
+    teha makseid jne.</p>
+</div>
 </template>
 <script>
 export default {
-  name: 'HomeviewGreeting'
+  name: 'HomeViewGreeting'
 }
 </script>
